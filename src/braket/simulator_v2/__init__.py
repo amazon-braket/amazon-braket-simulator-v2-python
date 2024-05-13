1a786ed7aca751d9d743c5ba02045b1c22f00b7d
Katharine Hyatt <hyatkath@amazon.com>
2024-05-13 17:44:33 -0400

    fix: import ordering

--- src/braket/simulator_v2/__init__.py
+++ src/braket/simulator_v2/__init__.py
@@ -12,6 +12,9 @@ from braket.simulator_v2.state_vector_simulator_v2 import (  # noqa: F401
 # seems to invalidate old methods
 from braket.circuits.circuit import Circuit
 from braket.circuits.serialization import IRType
+
+from ._version import __version__  # noqa: F401
+
 c = Circuit().h(0).cnot(0, 1).rx(0, 0.1).ry(1, 0.2).rz(0, 0.1)
 c.x(0).y(1).z(0).zz(0, 1, 0.2).xx(0, 1, 0.1).yy(0, 1, 0.1).xy(0, 1, 0.2)
 c.swap(1, 0).ecr(0, 1).gpi(0, 0.1).gpi2(1, 0.2).ms(0, 1, 0.1, 0.2, 0.3)
@@ -25,5 +28,3 @@ c.bit_flip(0, 0.1).phase_flip(0, 0.2).amplitude_damping(1, 0.1).phase_damping(0,
 c.two_qubit_dephasing(0, 1, 0.2).depolarizing(1, 0.2).two_qubit_depolarizing(1, 0, 0.1)
 device = DensityMatrixSimulatorV2()
 device.run(c.to_ir(IRType.OPENQASM), shots=100)
-
-from ._version import __version__  # noqa: F401
