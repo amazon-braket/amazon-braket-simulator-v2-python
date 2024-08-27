f550cabd5532d39a9e3dfa134ebff4080f86c670
Katharine Hyatt <67932820+kshyatt-aws@users.noreply.github.com>
2024-08-27 13:28:55 -0400

    Update src/braket/simulator_v2/julia_workers.py
    Co-authored-by: Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>

--- src/braket/simulator_v2/julia_workers.py
+++ src/braket/simulator_v2/julia_workers.py
@@ -54,14 +54,11 @@ def translate_and_run_multiple(
 ) -> List[str]:
     jl = sys.modules["juliacall"].Main
     irs = [program.source for program in programs]
-    is_single_input = isinstance(inputs, dict) or len(inputs) == 1
     py_inputs = {}
-    if (is_single_input and isinstance(inputs, dict)) or not is_single_input:
+    if len(inputs) > 1 or isinstance(inputs, dict)):
         py_inputs = [inputs.copy() for _ in range(len(programs))]
-    elif is_single_input and not isinstance(inputs, dict):
+    elif len(inputs) == 1:
         py_inputs = [inputs[0].copy() for _ in range(len(programs))]
-    else:
-        py_inputs = inputs
     full_inputs = []
     for p_ix, program in enumerate(programs):
         if program.inputs:
