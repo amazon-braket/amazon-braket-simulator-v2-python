aebe4faa65f13933f7150b7b9827c8d8122dbd43
shah-rushil <runshah@amazon.com>
2025-07-14 15:24:52 -0700

    support for julia branched MCM local simulator

--- src/braket/simulator_v2/__init__.py
+++ src/braket/simulator_v2/__init__.py
@@ -4,5 +4,8 @@ from braket.simulator_v2.density_matrix_simulator_v2 import (  # noqa: F401
 from braket.simulator_v2.state_vector_simulator_v2 import (  # noqa: F401
     StateVectorSimulatorV2,
 )
+from braket.simulator_v2.state_vector_simulator_branched import (
+    StateVectorSimulatorBranched
+)
 
 from ._version import __version__  # noqa: F401
